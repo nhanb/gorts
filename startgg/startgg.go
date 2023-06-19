5e64ca62ae9f2fa512601268e6ea6a6df6651e1e
Nhân <hi@imnhan.com>
2023-06-19 21:44:37 +0700

    WIP players
    Got country name-to-code mappings. Now let's actually fetch them.

---
+++ startgg/startgg.go
@@ -0,0 +1,80 @@
+package startgg
+
+import (
+	"bytes"
+	_ "embed"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const STARTGG_URL = "https://api.start.gg/gql/alpha"
+
+type Player struct {
+	GamerTag string
+	Prefix   string
+}
+
+type GraphQL struct {
+	Query     string   `json:"query"`
+	Variables struct{} `json:"variables"`
+}
+
+func FetchPlayers(token string, tourneySlug string) ([]Player, error) {
+	query := `
+{
+  tournament(slug: "%s") {
+    participants(query: {page: %d, perPage: 500}) {
+      nodes {
+        entrants {
+          event {
+            slug
+            name
+          }
+          team {
+            name
+          }
+        }
+        gamerTag
+        prefix
+        user {
+          location {
+            country
+          }
+        }
+      }
+    }
+  }
+}
+`
+	body, err := json.Marshal(GraphQL{
+		Query:     fmt.Sprintf(query, tourneySlug, 1),
+		Variables: struct{}{},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest("POST", STARTGG_URL, bytes.NewBuffer(body))
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("User-Agent", "GORTS/0.2")
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("fetch players: %w", err)
+	}
+	defer resp.Body.Close()
+
+	respdata, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(">>>>", string(respdata))
+
+	//var res map[string]interface{}
+	//json.NewDecoder(resp.Body).Decode(&res)
+	//fmt.Println(res["json"])
+	return nil, nil
+}
